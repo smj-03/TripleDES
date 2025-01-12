8d80fd2428f08eede10836fad6ee9a99ff822817
Szymon Chwastek <chwastekszymon@gmail.com>
2025-01-12 12:34:53 +0100

    feat: separate function for subkeys and encryption

--- main.c
+++ main.c
@@ -11,25 +11,49 @@ extern unsigned char EBitSelection[48];
 extern unsigned char P[32];
 extern unsigned char S[8][4][16];
 
+void createSubkeys(const _Bool*, _Bool subkeys[16][48]);
+void reverseSubkeys(_Bool subkeys[16][48]);
+void desEncryption(const _Bool*, const _Bool subkeys[16][48], _Bool*);
+
 int main(void) {
     const _Bool message[64] = {0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 0, 0, 1, 0, 0, 1, 1, 0, 1, 0, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 0, 1, 1, 1, 1};
     const _Bool key[64] = {0, 0, 0, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 0, 0, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 1};
+
+    _Bool subkeys[16][48];
     _Bool cipher[64];
 
     // STEP 1 - CREATE 16 SUB KEYS
+    createSubkeys(key, subkeys);
+
+    // STEP 2 - ENCODE BLOCKS OF DATA
+    desEncryption(message, subkeys, cipher);
+
+    unsigned char messageHex[16];
+    vector2Hex(message, 64, messageHex);
+    print_HexVector(messageHex, 16);
+
+    unsigned char cipherHex[16];
+    vector2Hex(cipher, 64, cipherHex);
+    print_HexVector(cipherHex, 16);
+
+    reverseSubkeys(subkeys);
+    _Bool decryptedMessage[64];
+    unsigned char decryptedHex[16];
+    desEncryption(cipher, subkeys, decryptedMessage);
+    vector2Hex(decryptedMessage, 64, decryptedHex);
+    print_HexVector(decryptedHex, 16);
+
+    return 0;
+}
+
+void createSubkeys(const _Bool* key, _Bool subkeys[16][48]) {
     _Bool keyPlus[56];
     _Bool keyL[17][28];
     _Bool keyR[17][28];
     _Bool keyBeforePC2[16][56];
-    _Bool keyAfterPC2[16][48];
 
     performPC1(key, PC1, keyPlus);
-    printf("PC1\n");
-    print_BoolVector(keyPlus, 56);
-
     partitionRL(keyPlus, 56, keyR[0], keyL[0]);
-    print_BoolVector(keyL[0], 28);
-    print_BoolVector(keyR[0], 28);
 
     for (unsigned char i = 1; i <= 16; i++) {
         shift2Left(keyR[i - 1], nLeftShift[i - 1], keyR[i]);
@@ -37,40 +61,35 @@ int main(void) {
         joinRL(keyBeforePC2[i - 1], 56, keyR[i], keyL[i]);
     }
 
-    print_BoolVector(keyBeforePC2[0], 56);
-    print_BoolVector(keyBeforePC2[15], 56);
-
     for (int i = 0; i < 16; i++)
-        performPC2(keyBeforePC2[i], PC2, keyAfterPC2[i]);
+        performPC2(keyBeforePC2[i], PC2, subkeys[i]);
+}
 
-    print_BoolVector(keyAfterPC2[0], 48);
-    print_BoolVector(keyAfterPC2[15], 48);
+void reverseSubkeys(_Bool subkeys[16][48]) {
+    for (int i = 0; i < 8; i++) {
+        _Bool temp[48];
+        for (int j = 0; j < 48; j++) {
+            temp[j] = subkeys[i][j];
+            subkeys[i][j] = subkeys[15 - i][j];
+            subkeys[15 - i][j] = temp[j];
+        }
+    }
+}
 
-    // STEP 2 - ENCODE BLOCKS OF DATA
+void desEncryption(const _Bool* message, const _Bool subkeys[16][48], _Bool* cipher) {
     _Bool messageL[17][32];
     _Bool messageR[17][32];
     _Bool outputF[32] = {0};
     _Bool preCrypto[64];
 
     performIP(message, IP, messageR[0], messageL[0]);
-    print_BoolVector(messageL[0], 32);
-    print_BoolVector(messageR[0], 32);
 
     for (int i = 1; i <= 16; i++) {
         memcpy(messageL[i], messageR[i - 1], 32 * sizeof(messageR[i][0]));
-        functionF(messageR[i - 1], keyAfterPC2[i - 1], EBitSelection, S, P, outputF);
+        functionF(messageR[i - 1], subkeys[i - 1], EBitSelection, S, P, outputF);
         XOR(messageL[i - 1], outputF, 32, messageR[i]);
     }
 
     joinRL(preCrypto, 64, messageL[16], messageR[16]);
-    print_BoolVector(preCrypto, 64);
-
     performIPInverse(preCrypto, IPInv, cipher);
-    print_BoolVector(cipher, 64);
-
-    unsigned char cipherHex[16];
-    vector2Hex(cipher, 64, cipherHex);
-    print_HexVector(cipherHex, 16);
-
-    return 0;
 }
